40a1d81de71466d43255ebb66f071c9df66197c3
Brett Tanner <bretttanner171@gmail.com>
2024-02-23 20:08:38 +0900

    Add basic user tests

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,22 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 
-# Create your tests here.
+
+class CustomUserTests(TestCase):
+    def test_create_user(self):
+        User = get_user_model()
+        user = User.objects.create_user(
+            username="testuser",
+            email="3u9oK@example.com",
+            password="testpass123",
+        )
+        self.assertEqual(user.username, "testuser")
+
+    def test_create_superuser(self):
+        User = get_user_model()
+        admin_user = User.objects.create_superuser(
+            username="superadmin",
+            email="4nSjL@example.com",
+            password="testpass123",
+        )
+        self.assertTrue(admin_user.is_superuser)
