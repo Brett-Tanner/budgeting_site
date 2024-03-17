ab3c67428fdf28f4dc6f1ab512737733990ad7e9
Brett Tanner <bretttanner171@gmail.com>
2024-03-17 21:33:04 +0900

    Style login/registration forms

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,19 @@
 module.exports = {
 	content: [
 		"./css/*.css",
-		"./templates/*.html",
+		"./templates/**/*.html",
 		"./components/**/*.{js,ts,jsx,tsx}",
 	],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				"dark-blue": "#00296b",
+				"neutral-blue": "#003f88",
+				"light-blue": "#00509d",
+				"neutral-gold": "#fdc500",
+				"light-gold": "#ffd500",
+			},
+		},
 	},
 	plugins: [],
 };
