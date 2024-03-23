635580fd06e1b37e40d7fe8d18039d71c64a8d93
Brett Tanner <bretttanner171@gmail.com>
2024-03-24 01:01:37 +0900

    Auto-populate user association on budget

--- budgets/urls.py
+++ budgets/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path
-from .views import BudgetListView, BudgetDetailView, BudgetUpdateView, BudgetDeleteView
+from .views import (
+    BudgetListView,
+    BudgetDetailView,
+    BudgetUpdateView,
+    BudgetDeleteView,
+    BudgetCreateView,
+)
 
 urlpatterns = [
     path("", BudgetListView.as_view(), name="budgets"),
     path("<int:pk>/", BudgetDetailView.as_view(), name="budget"),
     path("<int:pk>/edit/", BudgetUpdateView.as_view(), name="edit_budget"),
     path("<int:pk>/delete/", BudgetDeleteView.as_view(), name="delete_budget"),
+    path("new/", BudgetCreateView.as_view(), name="new_budget"),
 ]
--- budgets/views.py
+++ budgets/views.py
@@ -1,7 +1,9 @@
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView, DeleteView, UpdateView
 from django.urls import reverse_lazy
+from django.shortcuts import render
 from .models import Budget
+from .forms import BudgetForm
 
 
 class BudgetListView(ListView):
@@ -12,6 +14,25 @@ class BudgetListView(ListView):
 class BudgetDetailView(DetailView):
     model = Budget
     template_name = "budgets/budget.html"
+
+
+class BudgetCreateView(CreateView):
+    model = Budget
+    fields = ["name", "start_date", "end_date", "income"]
+    template_name = "budgets/new.html"
+    form = BudgetForm()
+
+    def post(self, request, *args, **kwargs):
+        form = BudgetForm(request.POST)
+        if form.is_valid():
+            instance = form.save(commit=False)
+            print(request.user.id)
+            print(instance)
+            instance.user_id = request.user.id
+            print(instance.user)
+            print(instance.user_id)
+            instance.save()
+            return render(request, "budgets/budget.html", {"budget": instance})
 
 
 class BudgetUpdateView(UpdateView):
